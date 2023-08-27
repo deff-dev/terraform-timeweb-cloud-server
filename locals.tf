d18477b1b21ae4341597771a63301f7f82d793a8
Deff <vitaliy.kotov2017@mail.ru>
2023-08-27 13:56:19 +0300

    Move `locals`  to `main.tf`

--- locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  preset_id       = try(data.twc_presets.default[0].id, null)
-  configurator_id = try(data.twc_configurator.default[0].id, null)
-  ssh_keys_ids    = [for key in data.twc_ssh_keys.default : key.id]
-}
